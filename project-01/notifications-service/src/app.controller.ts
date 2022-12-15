a86abfd8bdbfa9e33f7471bb4f3420ea1907540a
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-15 20:11:41 -0300

    feat: developing some routes 'GET' and 'POST'

--- project-01/notifications-service/src/app.controller.ts
+++ project-01/notifications-service/src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+/**
+ * file: src/app.controller.ts
+ * date: 12/15/2022
+ * description: file responsible for the 'AppController' class
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
 
-@Controller()
+import { PrismaService } from './prisma.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Welcome to the platform! We are glad to have you here! :)',
+        category: 'new_user',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
---
+++ project-01/notifications-service/src/prisma.service.ts
@@ -0,0 +1,23 @@
+/**
+ * file: src/ prisma.service.ts
+ * date: 12/15/2022
+ * description: file responsible for the connection with the database
+ * using the Prisma ORM
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
