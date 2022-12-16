34e457456338b06f4cb0ec92ee957d7063993b0b
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-15 21:08:03 -0300

    feat: including field validations - 'create-notification-body.ts'

--- project-01/notifications-service/src/app.controller.ts
+++ project-01/notifications-service/src/app.controller.ts
@@ -8,6 +8,7 @@
 import { PrismaService } from './prisma.service';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
 @Controller('notifications')
 export class AppController {
@@ -19,7 +20,7 @@ export class AppController {
   }
 
   @Post()
-  async create(@Body() body: any) {
+  async create(@Body() body: CreateNotificationBody) {
     const { content, category, recipientId } = body;
 
     await this.prisma.notification.create({
--- project-01/notifications-service/src/create-notification-body.ts
+++ project-01/notifications-service/src/create-notification-body.ts
@@ -6,8 +6,17 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
 export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
   recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 240)
   content: string;
+
+  @IsNotEmpty()
   category: string;
 }
