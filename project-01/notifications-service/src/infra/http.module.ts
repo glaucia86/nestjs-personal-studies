6ff016631458c5d59a9587ee1be9b22d7fa888f8
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-16 19:57:28 -0300

    refactor: organizing the project

---
+++ project-01/notifications-service/src/infra/http.module.ts
@@ -0,0 +1,13 @@
+/**
+ * file: src/infra/http.module.ts
+ * date: 12/16/2022
+ * description: file responsible for the http module
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+})
+export class HttpModule { }
