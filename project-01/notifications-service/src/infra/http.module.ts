4349df64584c4fef7f60bcf1ae9bbc27b09909b6
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-20 19:44:34 -0300

    refactor: refactoring the application

--- project-01/notifications-service/src/infra/http.module.ts
+++ project-01/notifications-service/src/infra/http.module.ts
@@ -6,8 +6,10 @@
  */
 
 import { Module } from '@nestjs/common';
+import { NotificationsController } from './http/controllers/notifications.controller';
 
 @Module({
   imports: [],
+  controllers: [NotificationsController],
 })
 export class HttpModule { }
