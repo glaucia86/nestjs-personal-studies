db8e6e09f9353d135d1fc0a439158219263cec17
glaucia86 <glaucia_lemos86@hotmail.com>
2023-02-23 10:05:39 -0300

    chore: including the 'Role.Admin' all user routes

--- project-01/api/src/user/user.controller.ts
+++ project-01/api/src/user/user.controller.ts
@@ -21,37 +21,45 @@ import {
 } from './dto/index';
 import { UserService } from './user.service';
 import { ParamId } from 'src/decorators/param.id.decorator';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
 
 @Controller('users')
 export class UserController {
 
   constructor(private readonly userService: UserService) { }
 
+  @Roles(Role.Admin)
   @Post()
   async create(@Body() data: CreateUserDTO) {
     return this.userService.createUser(data);
   }
 
+  @Roles(Role.Admin)
   @Get()
   async list() {
     return this.userService.listUsers();
   }
 
+  @Roles(Role.Admin)
   @Get(':id')
   async readById(@ParamId() id: number) {
     return this.userService.listUserById(id);
   }
 
+  @Roles(Role.Admin)
   @Put(':id')
   async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
     return this.userService.updateUser(id, data);
   }
 
+  @Roles(Role.Admin)
   @Patch(':id')
   async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number) {
     return this.userService.updateUserPartial(id, data);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   async delete(@ParamId() id: number) {
     return this.userService.deleteUserById(id);
