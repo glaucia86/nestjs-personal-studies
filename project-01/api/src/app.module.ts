63634d3af8e4c81849cdbd4eaaf0892b602d714d
glaucia86 <glaucia_lemos86@hotmail.com>
2023-02-27 10:06:43 -0300

    feat: development throttler logic

--- project-01/api/src/app.module.ts
+++ project-01/api/src/app.module.ts
@@ -1,16 +1,26 @@
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core'
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { forwardRef, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 100,
+    }),
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule)
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
   exports: [AppService]
 })
 export class AppModule { }
