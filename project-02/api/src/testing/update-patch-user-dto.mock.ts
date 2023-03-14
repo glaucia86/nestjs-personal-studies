6a76d3690e493ecf2c986c68a9d14a0280a9f538
glaucia86 <glaucia_lemos86@hotmail.com>
2023-03-14 11:17:54 -0300

    chore: develop update user method test

---
+++ project-02/api/src/testing/update-patch-user-dto.mock.ts
@@ -0,0 +1,14 @@
+/**
+ * file: src/testing/update-patch-user-dto.mock.ts
+ * date: 03/14/2023
+ * description: file responsible for 
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { Role } from '../enums/role.enum';
+import { UpdatePatchUserDTO } from '../user/dto/update-patch-user.dto';
+
+
+export const updatePatchUserDTO: UpdatePatchUserDTO = {
+  role: Role.Admin,
+}
