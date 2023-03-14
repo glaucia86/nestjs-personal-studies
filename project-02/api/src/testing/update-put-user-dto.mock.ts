08a98d07f6eeac9ff71f8017c6c75f234228adb2
glaucia86 <glaucia_lemos86@hotmail.com>
2023-03-14 11:18:30 -0300

    chore: develop patch user method test

---
+++ project-02/api/src/testing/update-put-user-dto.mock.ts
@@ -0,0 +1,17 @@
+/**
+ * file: src/testing/update-put-user-dto.mock.ts
+ * date: 03/14/2023
+ * description: file responsible for 
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { Role } from '../enums/role.enum';
+import { UpdatePutUserDTO } from './../user/dto/update-put-user.dto';
+
+export const updatePutUserDTO: UpdatePutUserDTO = {
+  name: 'Glaucia Lemos',
+  email: 'glaucia.lemos@email.com',
+  birthday: '2000-10-24',
+  password: '123456',
+  role: Role.Admin,
+}
